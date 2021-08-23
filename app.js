3ce5e866fc3b5b4b882b64724473716730bf6095
MohammedFurqhan <75026258+MohammedFurqhan@users.noreply.github.com>
2021-08-23 18:29:04 +0530

    feat : image carousal in JS

--- app.js
+++ app.js
@@ -16,6 +16,32 @@ window.onscroll = function()
     }
 }
 
+// Button Top
 btnScroll.addEventListener("click" , function(){
     window.scrollTo(0 , 0);//(left , top)
 });
+
+var i = 0 ;
+var images =[];
+var time = 300;
+// var slide = document.querySelector(".slide")
+
+// Image list
+
+images[0] = '/images/hero1.svg';
+images[1] = '/images/hero2.svg';
+images[2] = '/images/hero3.svg';
+
+function changeImg() {
+    document.slide.src= images[i];
+
+    if(i < images.length -1){
+        i++;
+    }else{
+        i=0;
+    }
+
+    setTimeout("changeImg()" , time);
+}
+
+window.onload = changeImg;
