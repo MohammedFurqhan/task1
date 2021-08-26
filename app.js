6aa55dc9979e81b44ae039a644bfb8860db7cd1c
MohammedFurqhan <75026258+MohammedFurqhan@users.noreply.github.com>
2021-08-26 14:34:08 +0530

    feat : validation part

--- app.js
+++ app.js
@@ -1,21 +1,45 @@
 // Form validation
 
-function validation(){
-    alert("hello");
-// var name = document.getElementById("Name").value;
-// var email = document.getElementById("email").value;
-// var phn = document.getElementById("phn").value;
+function validate(){
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var state = document.getElementById("state").value;
+    var phone = document.getElementById("phone").value;
+    var message = document.getElementById("message").value;
+    var error_message = document.getElementById("error_message");
     
-//     var text;
-//     alert(name);
-
-// //     if(name.length < 5){
-// //         text ="please enter valid Name";
-// //         errorElement.innerHTML =text;
-// //         return false;
-// // }
-}
-
+    error_message.style.padding = "10px";
+    
+    var text;
+    if(name.length < 5){
+      text = "Please Enter valid Name";
+      error_message.innerHTML = text;
+      return false;
+    }
+    if(email.indexOf("@") == -1 || email.length < 6){
+        text = "Please Enter valid Email";
+        error_message.innerHTML = text;
+        return false;
+      }
+    if(state.length < 5){
+      text = "Please Enter Correct State";
+      error_message.innerHTML = text;
+      return false;
+    }
+    if(isNaN(phone) || phone.length != 10){
+      text = "Please Enter valid Phone Number";
+      error_message.innerHTML = text;
+      return false;
+    }
+    
+    if(message.length <= 140){
+      text = "Please Enter More Than 140 Characters";
+      error_message.innerHTML = text;
+      return false;
+    }
+    alert("Form Submitted Successfully!");
+    return true;
+  }
 console.log("Hello")
 var nav = document.querySelector(".nav")
 var navmid = document.querySelector(".nav")
